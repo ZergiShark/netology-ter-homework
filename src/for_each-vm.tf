ef5801b47c44efb7f693d4e3ee89fc28f359dd33
Azamat Irnazarov <96770674+ZergiShark@users.noreply.github.com>
2023-11-26 21:31:10 +0500

    Update for_each-vm.tf

--- src/for_each-vm.tf
+++ src/for_each-vm.tf
@@ -1,50 +1,41 @@
 resource "yandex_compute_instance" "fe_instance" {
-
-depends_on = [ yandex_compute_instance.web ]
-
+  depends_on = [yandex_compute_instance.web]
   for_each = { for vm in local.vms_fe: "${vm.vm_name}" => vm }
   name = each.key
   platform_id = "standard-v1"
   resources {
- 	cores     	= each.value.cpu
- 	memory    	= each.value.ram
- 	core_fraction = each.value.frac
+    cores       = each.value.cpu
+    memory      = each.value.ram
+    core_fraction = each.value.frac
   }
-
   boot_disk {
-	initialize_params {
-  	image_id = "fd8g64rcu9fq5kpfqls0"
-	}
+    initialize_params {
+      image_id = var.image_id
+    }
   }
-
   network_interface {
-	subnet_id = yandex_vpc_subnet.develop.id
-	nat   	= true
+    subnet_id = var.subnet_id
+    nat     = true
   }
-
   metadata = {
-#	ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-	ssh-keys = local.ssh
+    ssh-keys = local.ssh
   }
 }
 
 locals {
   vms_fe = [
-	{
-   	vm_name = "vm-1"
-   	cpu 	= 2
-   	ram 	= 1
-   	frac	= 5
-	},
-	{
-   	vm_name = "vm-2"
-   	cpu 	= 2
-   	ram 	= 1
-   	frac	= 5
-	}
+    {
+      vm_name = "vm-1"
+      cpu   = 2
+      ram   = 1
+      frac  = 5
+    },
+    {
+      vm_name = "vm-2"
+      cpu   = 2
+      ram   = 1
+      frac  = 5
+    }
   ]
-}
-
-locals {
-  ssh = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  ssh = "ubuntu:${file(var.ssh_key_path)}"
 }
